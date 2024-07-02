20f39049d8e2d477d76cf80c4f9f00f62b68b6c7
NadyaGus <ftotkexit@gmail.com>
2024-07-02 16:09:05 +0300

    build: add new rule for eslint

--- class-components/src/utils/ErrorBoundary.tsx
+++ class-components/src/utils/ErrorBoundary.tsx
@@ -1,35 +1,34 @@
-import { Component } from "react";
-import { type ErrorInfo } from "react-dom/client";
+import { Component } from 'react';
+import { type ErrorInfo } from 'react-dom/client';
 
-type Props = {
-    children?: React.ReactNode,
+interface Props {
+  children?: React.ReactNode;
 }
 
-type State = {
-    error: Error | null
-    hasError: boolean
+interface State {
+  error: Error | null;
+  hasError: boolean;
 }
 
 export class ErrorBoundary extends Component<Props, State> {
-
-    public state: State = { error: null, hasError: false };
-    constructor(props: Props) {
-      super(props);
-    }
-
-    static getDerivedStateFromError(error: Error): State {
-      return { error, hasError: true };
-    }
-
-    componentDidCatch(error: Error, info: ErrorInfo): void {
-    console.error(error, info)
-    }
-
-    render(): React.ReactNode {
-      if (this.state.hasError) {
-        return <h1>Sorry. Something went wrong...</h1>
-      }
-
-      return this.props.children;
-    }
+  public state: State = { error: null, hasError: false };
+  constructor(props: Props) {
+    super(props);
   }
+
+  static getDerivedStateFromError(error: Error): State {
+    return { error, hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo): void {
+    console.error(error, info);
+  }
+
+  render(): React.ReactNode {
+    if (this.state.hasError) {
+      return <h1>Sorry. Something went wrong...</h1>;
+    }
+
+    return this.props.children;
+  }
+}
