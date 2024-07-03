c529a569c7b08f704398dbfabf2bf2d862bc6cce
NadyaGus <ftotkexit@gmail.com>
2024-07-03 21:14:41 +0300

    fix: fix error button

--- class-components/src/features/error/ErrorButton.tsx
+++ class-components/src/features/error/ErrorButton.tsx
@@ -1,10 +1,14 @@
 import { Component } from 'react';
 
 export class ErrorButton extends Component {
+  state = { hasError: false };
   handleClick(): void {
-    throw new Error('Error! Why are you doing this?');
+    this.setState({ hasError: true });
   }
   render(): React.ReactNode {
+    if (this.state.hasError) {
+      throw new Error('Something went wrong.');
+    }
     return <button onClick={() => this.handleClick()}>Error</button>;
   }
 }
