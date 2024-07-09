73bba8845e88a30ca4d02894fe3215e0925d839b
NadyaGus <ftotkexit@gmail.com>
2024-07-09 20:20:35 +0300

    refactor: change app, header, footer to function components

--- class-components/src/app/app.tsx
+++ class-components/src/app/app.tsx
@@ -1,16 +1,14 @@
-import { Component, type ReactNode } from 'react';
+import { type ReactNode } from 'react';
 
 import { SearchPage } from '@/pages/main-page';
 
 import classes from './app.module.css';
 
-export class App extends Component {
-  render(): ReactNode {
-    return (
-      <div className={classes.app}>
-        <SearchPage />
-        <div className={classes.background}></div>
-      </div>
-    );
-  }
-}
+export const App = (): ReactNode => {
+  return (
+    <div className={classes.app}>
+      <SearchPage />
+      <div className={classes.background}></div>
+    </div>
+  );
+};
