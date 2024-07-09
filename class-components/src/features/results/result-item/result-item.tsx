3a97e25e4e1dc7baa6c0b3474b08fbfe86353bf7
NadyaGus <ftotkexit@gmail.com>
2024-07-09 20:51:23 +0300

    refactor: change result-item

--- class-components/src/features/results/result-item/result-item.tsx
+++ class-components/src/features/results/result-item/result-item.tsx
@@ -1,18 +1,16 @@
-import { Component } from 'react';
+import type { ReactNode } from 'react';
 
 import type { Pokemon } from '@/api/get-items';
 
 import classes from './result-item.module.css';
 
-export class ResultItem extends Component<Pokemon> {
-  render(): React.ReactNode {
-    return (
-      <li className={classes.item}>
-        <h3 className={classes.title}>{this.props.name}</h3>
-        <img alt="alt" className={classes.image} src={this.props.images.small} />
-        <p className={classes.description}>Set: {this.props.set.name}</p>
-        <p className={classes.description}>Series: {this.props.set.series}</p>
-      </li>
-    );
-  }
-}
+export const ResultItem = (props: Pokemon): ReactNode => {
+  return (
+    <li className={classes.item}>
+      <h3 className={classes.title}>{props.name}</h3>
+      <img alt="alt" className={classes.image} src={props.images.small} />
+      <p className={classes.description}>Set: {props.set.name}</p>
+      <p className={classes.description}>Series: {props.set.series}</p>
+    </li>
+  );
+};
