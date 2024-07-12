8cbb4db1de8fadddc0047528038447dbfd19c0f2
NadyaGus <ftotkexit@gmail.com>
2024-07-12 18:51:50 +0300

    feat: add close button

--- class-components/src/features/detailed-card/detailed-card.tsx
+++ class-components/src/features/detailed-card/detailed-card.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
 
 import type { Pokemon } from '@/api/get-items';
 
@@ -11,12 +12,13 @@ interface DetailedCardProps {
 export const DetailedCard = (props: DetailedCardProps): ReactNode => {
   return (
     <div className={classes.container}>
+      <Link to={'/'}>
+        <button>close</button>
+      </Link>
       <div className={classes.card}>
         <img alt={props.pokemon.name} className={classes.image} src={props.pokemon.images?.small} />
         <div className={classes.info}>
           <h1 className={classes.title}>{props.pokemon.name}</h1>
-          <div />
-          <div />
         </div>
       </div>
     </div>
