01ab24552b29a65feea4cbbd2ec20fb2a1f7db8e
NadyaGus <ftotkexit@gmail.com>
2024-07-18 15:03:57 +0300

    refactor: move router and error boundary to providers

--- class-components/src/app/app.tsx
+++ class-components/src/app/app.tsx
@@ -1,8 +1,8 @@
 import { type ReactNode } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
-import { ErrorBoundary } from '@/shared/error-boundary/error-boundary';
-import { router } from '@/shared/router/router';
+import { ErrorBoundary } from './providers/error-boundary/error-boundary';
+import { router } from './providers/router/router';
 
 export const App = (): ReactNode => {
   return (
