17e5d65eedb4312cc831686057b03909f70019c0
NadyaGus <ftotkexit@gmail.com>
2024-07-22 13:17:38 +0300

    feat: add styles for dark theme

--- class-components/src/pages/not-found-page/not-found-page.tsx
+++ class-components/src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,9 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { clsx } from 'clsx';
+
+import { ThemeContext } from '@/app/providers/theme/theme';
 import { Background } from '@/components/background/background';
 import { Footer } from '@/components/footer/footer';
 import { Header } from '@/components/header/header';
@@ -9,9 +12,10 @@ import classes from './not-found-page.module.css';
 
 export const NotFoundPage = (): ReactNode => {
   const navigate = useNavigate();
+  const { theme } = useContext(ThemeContext);
 
   return (
-    <div className={classes.app}>
+    <div className={clsx(classes.app, classes[theme])}>
       <Header />
 
       <div className={classes.container}>
