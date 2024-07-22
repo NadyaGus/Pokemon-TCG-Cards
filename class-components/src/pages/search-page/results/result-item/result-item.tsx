63d92cc4a45e354257a0c4e896340e2043fcfc69
NadyaGus <ftotkexit@gmail.com>
2024-07-22 14:21:24 +0300

    feat: add checkboxes for items

--- class-components/src/pages/search-page/results/result-item/result-item.tsx
+++ class-components/src/pages/search-page/results/result-item/result-item.tsx
@@ -9,17 +9,20 @@ export const ResultItem = (props: Pokemon): ReactNode => {
   const [searchParams] = useSearchParams();
 
   return (
-    <Link
-      className={classes.link}
-      data-testid="item"
-      to={`/cards/${props.id}?page=${searchParams.get('page')}&pageSize=${searchParams.get('pageSize')}&name=${searchParams.get('name')}`}
-    >
-      <li className={classes.item}>
+    <li className={classes.item}>
+      <Link
+        className={classes.link}
+        data-testid="item"
+        to={`/cards/${props.id}?page=${searchParams.get('page')}&pageSize=${searchParams.get('pageSize')}&name=${searchParams.get('name')}`}
+      >
         <h3 className={classes.title}>{props.name}</h3>
         <img alt={props.name} className={classes.image} src={props.images.small} />
         <p className={classes.description}>Set: {props.set.name}</p>
-        <p className={classes.description}>Series: {props.set.series}</p>
-      </li>
-    </Link>
+      </Link>
+
+      <label className={classes.checkbox}>
+        Choose the card <input type="checkbox" />
+      </label>
+    </li>
   );
 };
