af3bb0ac1d215d2ba164fd050abb1493619979b1
NadyaGus <ftotkexit@gmail.com>
2024-07-24 17:07:07 +0300

    fix: fix tests for cards

--- class-components/src/app/app.test.tsx
+++ class-components/src/app/app.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import { expect, it } from 'vitest';
+import { expect, it, vi } from 'vitest';
 
 import { App } from './app';
 
 it('full app rendering', () => {
+  global.URL.createObjectURL = vi.fn();
+
   render(<App />);
 
   expect(screen.getByText('Pokémon TCG Cards')).toBeInTheDocument();
