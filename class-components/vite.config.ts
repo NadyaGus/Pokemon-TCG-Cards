ea2f3258fae4e3c7e1dc561a282546947e4e1981
NadyaGus <ftotkexit@gmail.com>
2024-07-26 17:30:17 +0300

    feat: change vite config

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  base: '',
+  base: './',
   plugins: [tsconfigPaths(), react()],
   publicDir: './public',
   test: {
