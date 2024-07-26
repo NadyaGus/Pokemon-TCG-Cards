6fd3c006aa7797149e6aa955d0b00a8acb2526e9
NadyaGus <ftotkexit@gmail.com>
2024-07-26 20:33:23 +0300

    refactor: try fix background load

--- src/shared/background/background.tsx
+++ src/shared/background/background.tsx
@@ -1,7 +1,13 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useContext } from 'react';
+
+import { clsx } from 'clsx';
+
+import { ThemeContext } from '@/app/providers/theme/theme';
 
 import classes from './background.module.css';
 
 export const Background = (): ReactNode => {
-  return <div className={classes.background}></div>;
+  const { theme } = useContext(ThemeContext);
+
+  return <div className={clsx(classes.background, classes[theme])}></div>;
 };
