c2b0459d10fee61b43aeabce8d75b648ee1af869
NadyaGus <ftotkexit@gmail.com>
2024-07-27 14:50:44 +0300

    fix: fix name for .env variable and add header for request

--- src/app/api/pokemonApi.ts
+++ src/app/api/pokemonApi.ts
@@ -1,11 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { API_URL } from '@/app/variables';
+import { API_KEY, API_URL } from '@/app/variables';
 
 import type { ResponseData, ResponseDataCard } from './types';
 
 export const pokemonApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_URL,
+    prepareHeaders: (headers) => {
+      headers.set('X-API-KEY', API_KEY);
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getPokemon: builder.query<ResponseDataCard, string>({
       query: (id) => `/${id}`,
