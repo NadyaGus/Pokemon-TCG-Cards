098f75acc89e131b638485aee7e4f40963d82199
NadyaGus <ftotkexit@gmail.com>
2024-07-28 01:29:08 +0300

    fix: fix localstorage hook

--- src/app/hooks/useLocalStorage.tsx
+++ src/app/hooks/useLocalStorage.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react';
 
-import { LS_KEY } from '@/app/variables';
+import { LS_KEY_SEARCH } from '@/app/variables';
+
+const initialValue = (): string => {
+  return localStorage.getItem(LS_KEY_SEARCH) ?? '';
+};
 
 export const useLocalStorage = (): [string, React.Dispatch<React.SetStateAction<string>>] => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(initialValue());
 
   useEffect(() => {
-    setValue(localStorage.getItem(LS_KEY) ?? '');
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(LS_KEY, value);
+    localStorage.setItem(LS_KEY_SEARCH, value);
   }, [value]);
 
   return [value, setValue];
