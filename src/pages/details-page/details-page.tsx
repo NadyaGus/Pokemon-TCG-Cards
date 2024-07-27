6c4b2e1bcc51e17c5cb7fc2cd33d2ad6fffa7c8f
NadyaGus <ftotkexit@gmail.com>
2024-07-28 00:29:15 +0300

    refactor: remove details card

--- src/pages/details-page/details-page.tsx
+++ src/pages/details-page/details-page.tsx
@@ -1,14 +1,68 @@
 import { type ReactNode } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
 
 import { useGetPokemonQuery } from '@/app/api/pokemonApi';
 
-import { DetailedCard } from './detailed-card/detailed-card';
+import classes from './details-page.module.css';
 
 export const DetailsPage = (): ReactNode => {
   const { cardId } = useParams();
 
   const { data } = useGetPokemonQuery(cardId ?? '');
 
-  return <DetailedCard pokemon={data?.data} />;
+  const [searchParams] = useSearchParams();
+
+  const pokemon = data?.data;
+
+  if (!pokemon) {
+    return (
+      <div className={classes.container}>
+        <Link
+          className={classes.closeArea}
+          to={`/?name=${searchParams.get('name')}&page=${searchParams.get('page')}&pageSize=${searchParams.get('pageSize')}`}
+        />
+        <div className={classes.cardArea}>
+          <Link
+            className={classes.closeButton}
+            to={`/?name=${searchParams.get('name')}&page=${searchParams.get('page')}&pageSize=${searchParams.get('pageSize')}`}
+          >
+            <button>Close</button>
+          </Link>
+          <h2>Card not found</h2>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.container}>
+      <Link
+        className={classes.closeArea}
+        to={`/?name=${searchParams.get('name')}&page=${searchParams.get('page')}&pageSize=${searchParams.get('pageSize')}`}
+      />
+
+      <div className={classes.cardArea}>
+        <div className={classes.cardContainer}>
+          <Link
+            className={classes.closeButton}
+            to={`/?name=${searchParams.get('name')}&page=${searchParams.get('page')}&pageSize=${searchParams.get('pageSize')}`}
+          >
+            <button>Close</button>
+          </Link>
+
+          <div className={classes.card}>
+            <img alt={pokemon.name} className={classes.image} src={pokemon.images?.large} />
+            <div className={classes.info}>
+              <h2 className={classes.title}>{pokemon.name}</h2>
+              <p className={classes.description}>Set: {pokemon.set?.name}</p>
+              <p className={classes.description}>Series: {pokemon.set?.series}</p>
+              <p className={classes.description}>Supertype: {pokemon.supertype}</p>
+              <p className={classes.description}>Types: {pokemon.types}</p>
+              <p className={classes.description}>HP: {pokemon.hp}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
